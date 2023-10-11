2f6f0075dac1de3507877ca3a34c80fa6f1fe647
EmiN364 <68005487+EmiN364@users.noreply.github.com>
2023-10-11 01:47:08 -0300

    docs: :memo: Remove old docs, create docs directory and change docker to 2.0
    Co-Authored-By: gcandisano <102882560+gcandisano@users.noreply.github.com>
    Co-Authored-By: Theo Shlamovitz <87035324+Theo-Shlamo@users.noreply.github.com>
    Co-Authored-By: gcandisano <102882560+gcandisano@users.noreply.github.com>
    Co-Authored-By: Theo Shlamovitz <87035324+Theo-Shlamo@users.noreply.github.com>

--- docker.sh
+++ docker.sh
@@ -1,13 +1,13 @@
-# Check if docker arqui exists
-if [ ! "$(docker ps -a -q -f name=arqui)" ]; then
-    # Create docker arqui
-    docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name arqui agodio/itba-so:1.0
+# Check if docker so_tp2 exists
+if [ ! "$(docker ps -a -q -f name=so_tp2)" ]; then
+    # Create docker so_tp2
+    docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name so_tp2 agodio/itba-so:2.0
 fi
 
-docker start arqui
-docker exec -it arqui make clean -C/root/Toolchain
-docker exec -it arqui make clean -C/root/
-docker exec -it arqui make -C/root/Toolchain
-docker exec -it arqui env GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01" make -C/root/
-docker stop arqui
+docker start so_tp2
+docker exec -it so_tp2 make clean -C/root/Toolchain
+docker exec -it so_tp2 make clean -C/root/
+docker exec -it so_tp2 make -C/root/Toolchain
+docker exec -it so_tp2 env GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01" make -C/root/
+docker stop so_tp2
 bash run.sh
