fd7e5d8bae64d6d5362a44b33eb17580eba8281f
Theo-Shlamo <87035324+Theo-Shlamo@users.noreply.github.com>
2023-11-02 15:44:51 -0300

    feat: :test_tube: Creating pipes.c and pipes.h
    Co-Authored-By: EmiN364 <68005487+EmiN364@users.noreply.github.com>
    Co-Authored-By: gcandisano <102882560+gcandisano@users.noreply.github.com>

---
+++ Kernel/include/pipes.h
@@ -0,0 +1,30 @@
+#ifndef PIPES_H
+#define PIPES_H
+
+#include <stdint.h>
+
+#define PIPE_BUFFER_SIZE 1024
+#define MAX_PIPES 50
+#define EOF -1 
+
+typedef struct pipesType {
+    int name;
+    unsigned int readPos;
+    unsigned int writePos;
+    uint8_t eof;
+    int leftToRead;
+    char *pipeBuffer;
+    int readSemId;
+    int writeSemId;
+} pipesType;
+
+void pipesInit();
+int namedPipeCreate(int name);
+int anonPipeCreate();
+void pipeDestroy(int id);
+void sendEOF(int id);
+int getPipeId(int name);
+int readPipe(int id, char *dest, unsigned int count);
+int writePipe(int id, const char *src, unsigned int count);
+
+#endif
